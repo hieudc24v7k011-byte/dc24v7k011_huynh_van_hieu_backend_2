81025b62b34fc4d469ec104156bd8db717e9230b
hieudc24v7k011-byte <hieudc24v7k011@vlvh.ctu.edu.vn>
2026-04-29 18:01:45 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
+const ApiError = require("./app/api-error");
 const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
@@ -9,9 +10,20 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Contactbook application." });
+  res.json({ message: "Welcome Contactbook API" });
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+  next(new ApiError(404, "Resource not found"));
+});
+
+// eslint-disable-next-line no-unused-vars
+app.use((error, req, res, next) => {
+  res.status(error.statusCode || 500).json({
+    message: error.message || "Internal Server Error",
+  });
+});
 
 module.exports = app;
